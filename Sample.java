51cc4fe4ca0aa92797e889e8352be4b5b3db5d1f
Divyanshshiva <108665652+Divyanshshiva@users.noreply.github.com>
2024-08-22 14:09:08 +0530

    sample updated2

--- Sample.java
+++ Sample.java
@@ -2,6 +2,6 @@ class Sample
 {
 	public static void main(String args[])
 	{
-		System.out.println("cmrcet hyd in telangana ");
+		System.out.println("cmrcet hyd in telangana in kandlakoya ");
 	}
 }
